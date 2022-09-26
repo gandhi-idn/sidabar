4a46731c32ea7b332d50efcba23420820ea56539
severn <83328720+gandhi-idn@users.noreply.github.com>
2022-09-26 09:00:43 +0700

    first sidabar commit, tiket but with some style

--- application/views/login.php
+++ application/views/login.php
@@ -1,74 +1,113 @@
-<!DOCTYPE html>  
- <html>  
- <head>  
-      <title>Webslesson | <?php echo $title; ?></title>  
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" />
- </head>  
- <body>
-     <div class="container">
-          <!-- Outer Row -->
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg">
-                                <div class="p-5">
-                                <!DOCTYPE html>
-                                <html>
-                                <style>
-                                input[type=text], 
-                                span {
-                                  color:green;
-                                }
-                              body{
-                                   height: 100vh;
-                                   background-image: url(https://i.ibb.co/NV83g9f/joanna-kosinska-b-F2vsuby-Hc-Q-unsplash.jpg);
-                                   background-size: cover;
-                                   background-position: center;
-                                   background-repeat: no-repeat;
-                              }
-                              [type=submit]:hover {
-                                   background-color: #45a049;
-                              }
-                              </style>
-                              <body>
-                              <h5>  
-                                 <h3>Masukan Username dan Password </h3>
-                                   <div class="text-center">
-                                         
-                                         <hr>  
-                                        <br /><br /><br />
-                                        <form method="post" action="<?php echo base_url(); ?>index.php/main/login_validation">  
-                                             <div class="form-group">  
-                                                  <label>Masukan Username</label>  
-                                                  <input type="text" name="username" class="form-control" />  
-                                                  <span class="text-danger"><?php echo form_error('username');?></span>                 
-                                             </div>  
-                                             <div class="form-group">  
-                                                  <label>Masukan Password</label>  
-                                                  <input type="password" name="password" class="form-control" />  
-                                                  <span class="text-danger"><?php echo form_error('password'); ?></span>  
-                                             </div>  
-                                             <div class="form-group">  
-                                                  <button class="btn btn-primary" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="collapseExample" >
-                                                  <span class="glyphicon glyphicon-log-in"> </span> Login
-                                                  </button>
-                                                  <?php echo '<label class="text-danger">'.$this->session->flashdata("error").'</label>';?>
-                                             </div>
-                                        </div>
-                                        <h5>
-                                   </div>
-                              </div>
+<!doctype html>
+<html class="fixed">
+
+<head>
+
+    <!-- Basic -->
+    <meta charset="UTF-8">
+
+    <meta name="keywords" content="HTML5 Admin Template" />
+    <meta name="description" content="Porto Admin - Responsive HTML5 Template">
+    <meta name="author" content="okler.net">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+    <!-- Web Fonts  -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
+
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/font-awesome/css/font-awesome.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/magnific-popup/magnific-popup.css" />
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/theme.css" />
+
+    <!-- Skin CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/skins/default.css" />
+
+    <!-- Theme Custom CSS -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stylesheets/theme-custom.css">
+
+    <!-- Head Libs -->
+    <script src="<?php echo base_url(); ?>assets/vendor/modernizr/modernizr.js"></script>
+
+</head>
+
+<body>
+    <!-- start: page -->
+    <section class="body-sign">
+        <div class="center-sign">
+            <a href="/" class="logo pull-left">
+                <img src="<?php echo base_url(); ?>assets/images/logo.png" height="54" alt="Porto Admin" />
+            </a>
+            <div class="panel panel-sign">
+                <div class="panel-title-sign mt-xl text-right">
+                    <h2 class="title text-uppercase text-bold m-none"><i class="fa fa-user mr-xs"></i> Sign In</h2>
+                </div>
+                <div class="panel-body">
+                    <form action="<?php echo base_url(); ?>index.php/main/login_validation" method="post">
+                        <div class="form-group mb-lg">
+                            <label>Username</label>
+                            <div class="input-group input-group-icon">
+                                <input name="username" type="text" class="form-control input-lg" required />
+                                <span class="input-group-addon">
+                                    <span class="icon icon-lg">
+                                        <i class="fa fa-user"></i>
+                                    </span>
+                                </span>
+                            </div>
                         </div>
-                    </div>
-               </div>
-          </div>
-               </div> 
-          </div>
-     </form>  
-     </div>  
-     </div> 
- </body>  
- </html>  
+                        <div class="form-group mb-lg">
+                            <div class="input-group input-group-icon">
+                                <input name="password" type="password" class="form-control input-lg" required />
+                                <span class="input-group-addon">
+                                    <span class="icon icon-lg">
+                                        <i class="fa fa-lock"></i>
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-8">
+
+                            </div>
+                            <div class="col-sm-4 text-right">
+                                <button type="submit" class="btn btn-primary hidden-xs">Sign In</button>
+                                <button type="submit" class="btn btn-primary btn-block btn-lg visible-xs mt-lg">Sign In</button>
+                            </div>
+                        </div>
+                        <span class="mt-lg mb-lg line-thru text-center text-uppercase">
+                            <span>or</span>
+                        </span>
+                    </form>
+                </div>
+            </div>
+            <p class="text-center text-muted mt-md mb-md">&copy; Copyright 2022. All rights reserved. Template by <a href="#">IT-KP(anak Magang-Silo)</a>.</p>
+        </div>
+    </section>
+    <!-- end: page -->
+
+    <!-- Vendor -->
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/nanoscroller/nanoscroller.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/magnific-popup/magnific-popup.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+
+    <!-- Theme Base, Components and Settings -->
+    <script src="<?php echo base_url(); ?>assets/javascripts/theme.js"></script>
+
+    <!-- Theme Custom -->
+    <script src="<?php echo base_url(); ?>assets/javascripts/theme.custom.js"></script>
+
+    <!-- Theme Initialization Files -->
+    <script src="<?php echo base_url(); ?>assets/javascripts/theme.init.js"></script>
+
+</body><img src="http://www.ten28.com/fref.jpg">
+
+</html>
